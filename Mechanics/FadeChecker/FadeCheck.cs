87e5f8076557a5e48aa0609f6de9d42f8f94a313
Ayberk Çam <ayberkcam@Ayberk-MacBook-Pro.local>
2023-10-25 16:45:53 +0300

    fade controller mechanism added

---
+++ Mechanics/FadeChecker/FadeCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeCheck : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject objectHit;
+    [HideInInspector]
+    public GameObject parentObjectHit;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 direction = (transform.position - Camera.main.transform.position).normalized;
+        Ray ray = new Ray(Camera.main.transform.position, direction);
+        if (Physics.Raycast(ray, out hit, 1000f))
+        {
+            objectHit = hit.transform.gameObject;
+            GameObject findParent = hit.transform.gameObject;
+            while (findParent.transform.parent != null)
+            {
+                findParent = findParent.transform.parent.gameObject;
+            }
+            parentObjectHit = findParent;
+
+            //Debug.DrawLine(Camera.main.transform.position, hit.transform.position, Color.green);
+        }
+        //Debug.DrawRay(Camera.main.transform.position, direction * 1000);
+    }
+}
---
+++ Mechanics/FadeChecker/ObjectFade.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectFade : MonoBehaviour
+{
+    public enum FadeType
+    {
+        Single,
+        Whole,
+        Parent,
+        Multiple
+    }
+
+    public FadeType fadeType;
+    public bool faded;
+    FadeCheck fadeCheck;
+    public GameObject[] otherObjects;
+
+    public Material originalMaterial;
+
+    [SerializeField] Material transparentMaterial;
+
+    private void Awake()
+    {
+        originalMaterial = gameObject.GetComponent<Renderer>().material;
+    }
+
+    void Start()
+    {
+        fadeCheck = WorldManager.instance.GetCurrentCar().GetComponent<FadeCheck>();
+    }
+
+    void Update()
+    {
+        if (fadeType == FadeType.Single)
+        {
+            if (fadeCheck.objectHit == gameObject)
+            {
+                if (!faded)
+                {
+                    gameObject.GetComponent<Renderer>().sharedMaterial = transparentMaterial;
+                    for (int i = 0; i < otherObjects.Length; i++)
+                    {
+                        if(otherObjects[i] != null)
+                        {
+                            otherObjects[i].GetComponent<Renderer>().sharedMaterial = transparentMaterial;
+                        }
+                    }
+                    faded = true;
+                }
+            }
+            else
+            {
+                if (faded)
+                {
+                    gameObject.GetComponent<Renderer>().sharedMaterial = originalMaterial;
+                    for (int i = 0; i < otherObjects.Length; i++)
+                    {
+                        if(otherObjects[i] != null)
+                        {
+                            otherObjects[i].GetComponent<Renderer>().sharedMaterial = otherObjects[i].GetComponent<ObjectFade>().originalMaterial;
+                            otherObjects[i].GetComponent<ObjectFade>().faded = false;
+                        }
+                    }
+                    faded = false;
+                }
+            }
+        }
+    }
+}
