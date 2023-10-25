c10de299b3a9cd0036ec9158f839b79e7cb17de8
Ayberk Çam <ayberkcam@Ayberk-MacBook-Pro.local>
2023-10-25 11:26:16 +0300

    an example event helper added

---
+++ Helpers/EventHelper.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class EventHelper
+{
+    public static Action EmptyEvent;
+    public static Action<string> StringEvent;
+    public static Action<int> IntEvent;
+    public static Action<bool> BooleanEvent;
+    public static Action<ClassObj> ObjectEvent;
+    public static Action<string, int, int> MultipleInputEvent;
+}
