e8c6f960447d1f736c73a383eab00ad06dab1375
Ayberk Çam <ayberkcam@Ayberk-MacBook-Pro.local>
2023-10-26 12:09:47 +0300

    new pooler added

---
+++ Pooler/ObjectPooler.cs
@@ -0,0 +1,127 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPooler : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    #region Singleton
+    public static ObjectPooler Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+    #endregion
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+
+    private void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+
+
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new();
+
+            for (int i = 0; i < pool.size; i++)
+            {
+
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+            pool.tag = pool.prefab.name;
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject FindPrefab(string tag)
+    {
+        foreach (Pool pool in pools)
+        {
+            if (pool.tag == tag)
+            {
+                return pool.prefab;
+            }
+        }
+
+        return null;
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (poolDictionary == null)
+        {
+            poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        }
+
+        if (!poolDictionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with tag " + tag + " doesn't exist.");
+            return null;
+        }
+
+        if (poolDictionary[tag].Count < 1)
+        {
+            //New Objects added for pool
+            foreach (Pool pool in pools)
+            {
+                if (pool.tag == tag)
+                {
+                    for (int i = 0; i < pool.size; i++)
+                    {
+                        GameObject obj = Instantiate(pool.prefab);
+                        obj.SetActive(false);
+                        poolDictionary[tag].Enqueue(obj);
+                    }
+                }
+            }
+        }
+
+        GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+        objectToSpawn.SetActive(true);
+
+        IPooledObject pooledObject = objectToSpawn.GetComponent<IPooledObject>();
+
+        if (pooledObject != null)
+        {
+            pooledObject.OnObjectSpawn();
+        }
+
+        //poolDictionary[tag].Enqueue(objectToSpawn);
+        return objectToSpawn;
+    }
+
+    public void ReturnToPool(GameObject returnObject, string tag)
+    {
+        if (!poolDictionary[tag].Contains(returnObject))
+            poolDictionary[tag].Enqueue(returnObject);
+        returnObject.SetActive(false);
+    }
+
+    public Queue<GameObject> GetPool(string tag)
+    {
+        return poolDictionary[tag];
+    }
+
+
+}
